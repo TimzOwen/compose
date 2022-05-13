c47abf297e096cb3fc66f73b5c752283cf4fd55d
TimzOwen <timzowen@gmail.com>
2022-05-14 00:50:56 +0300

    updated the columns and modifiers:

--- app/src/main/java/com/timzowen/compose/MainActivity.kt
+++ app/src/main/java/com/timzowen/compose/MainActivity.kt
@@ -3,13 +3,15 @@ package com.timzowen.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.timzowen.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +19,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                myApp()
             }
         }
     }
 }
 
+// Reusable compose element
 @Composable
-fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary){
-        Text(text = "Hey $name")
+private fun myApp(){
+    Surface(color = MaterialTheme.colors.background){
+        Greeting(name = "Hello ")
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colors.primary) {
+        Column(modifier = Modifier.padding(28.dp)) {
+            Text(text = "Hello android $name")
+            Text(text="Testing bank $name")
+        }
     }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+private fun DefaultPreview() {
     ComposeTheme {
-        Greeting("Android")
+        myApp()
     }
 }
